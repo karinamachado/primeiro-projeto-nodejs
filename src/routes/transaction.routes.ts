63cd4ba8d9bf6cbfc8d9dc4df33a7712e87c19f4
karinamachado <karinamachado@msn.com>
2021-01-28 17:49:38 -0300

    challenge finalized

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,23 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const transactions = transactionsRepository.all();
+    
+    const balance = transactionsRepository.getBalance();
+
+    return response.json({
+      transactions,
+      balance,
+    })
+    
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,9 +25,23 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    
+    const {title, value, type} = request.body;
+// criando transacao, cuida da regra de negócio
+    const createTransaction = new CreateTransactionService(
+      transactionsRepository,
+    );
+//executar a transacao com o metodo execute
+    const transaction = createTransaction.execute({
+      title,
+      value,
+      type,
+    });
+    // retorna o valor da transaction
+    return response.json(transaction);
+
   } catch (err) {
-    return response.status(400).json({ error: err.message });
+    return response.status(400).json({error: err.message });
   }
 });
 
